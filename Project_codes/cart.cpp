cef46cd027ad554f70e65370c5ce0f1a5ea2fa51
jacobennathaniel <113477327+LearningStff@users.noreply.github.com>
2025-04-07 17:33:01 +0000

    Updated the codes overall

---
+++ Project_codes/cart.cpp
@@ -0,0 +1,67 @@
+#define CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+
+#include "cart.h"
+using namespace std;
+
+Cart::Cart(){
+  m_products = nullptr;
+  m_quantities = nullptr;
+  m_totalItems = 0;
+  m_totalPrice = 0.0;
+}
+
+Cart::~Cart(){
+  delete [] m_products;
+  delete [] m_quantities;
+  m_products = nullptr;
+  m_quantities = nullptr;
+}
+
+void Cart::addProductToCart(const Product& product, int quantity){
+  
+}
+
+
+void Cart::listItemsInCart() const {
+    // list all items in the cart
+    // display the name, price, quantity, and total price of each item
+    for (int i = 0; i < m_totalItems; i++){
+      cout << m_products[i].getName() << "," << m_quantities[i] << " units, $"
+           << m_products[i].getPrice() << " per unit"; 
+    }
+}
+
+// checkout function'
+bool Cart::checkout() {
+  if (m_totalItems == 0) {
+    return false;  // return false if the cart is empty 
+  } else {
+    return true;
+  }
+   clearCart();
+}
+
+// clear cart function
+void Cart::clearCart() {
+  m_totalItems = 0;
+  m_totalPrice = 0.0;
+}
+
+// other functions (maybe useful)
+double Cart::calculateTotalPrice() const {
+    // calculate the total price of all items in the cart
+  double subtotal = 0.0;
+  for (int i = 0; i < m_totalItems; i++){
+     double totalItems = m_products[] m_products[i].getPrice() * m_quantities[i]; 
+     subtotal = subtotal + totalItems;
+  }
+  return subtotal * 1.13;
+}
+
+int Cart::getTotalItems() const {
+  // return the total number of items in the cart
+  return m_totalItems;
+}
